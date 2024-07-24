dd3f355b135dd21d628d646d42b66a4f3d26b5f0
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-07-24 17:17:23 +0500

    Finished

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,18 +1,58 @@
-export default function addTopic() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AddTopic() {
+  // Corrected function name to start with a capital letter
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and Description are required!");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
         type="text"
         placeholder="Topic Title"
         className="border border-slate-500 px-8 py-2"
       />
       <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         type="text"
         placeholder="Topic Description"
         className="border border-slate-500 px-8 py-2"
       />
-
-      <button className="bg-green-600 font-bold px-5 py-3 w-fit">
+      <button type="submit" className="bg-green-600 font-bold px-5 py-3 w-fit">
         Add Topic
       </button>
     </form>
